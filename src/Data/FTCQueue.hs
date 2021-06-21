21becb0075a39087bee10ed62c803ad5d7a7f900
Mike Delmonaco <mthundershock@gmail.com>
2021-06-21 11:18:04 -0400

    starting FTCQueue, stuck on check empty case

--- src/Data/FTCQueue.hs
+++ src/Data/FTCQueue.hs
@@ -4,15 +4,25 @@ module Data.FTCQueue where
 -- pop from here
 data Kleisli m a b where
     Singleton :: (a -> m b) -> Kleisli m a b
-    ComposeL :: (a -> m b) -> Kleisli m b c -> Kleisli m a c
+    Compose :: (a -> m b) -> Kleisli m b c -> Kleisli m a c
 
 -- push to here
 data KleisliReverse m a b where
     SingletonReverse :: (a -> m b) -> KleisliReverse m a b
-    ComposeR :: KleisliReverse m a b -> (b -> m c) -> KleisliReverse m a c
+    ComposeReverse :: KleisliReverse m b c -> (a -> m b) -> KleisliReverse m a c
 
 data KleisliQueue m a b
-    = KleisliQueue !Int (Kleisli m a b) !Int (KleisliReverse m a b)
+    = KleisliQueue !Int (KleisliReverse m a b) !Int (Kleisli m a b)
 
+reverseKleisli :: KleisliReverse m a b -> Kleisli m a b
+reverseKleisli (SingletonReverse k) = Singleton k
+reverseKleisli (ComposeReverse ks k) = Compose k (reverseKleisli ks)
+
+appendKleisli :: Kleisli m a b -> Kleisli m b c -> Kleisli m a c
+appendKleisli (Singleton k) ks = Compose k ks
+appendKleisli (Compose k ks) ks' = Compose k (appendKleisli ks ks')
+
+-- can't just append and empty bc there is no empty kleisli. maybe allow empty reverse, but not empty forward? Only problem with empty is appending anyway
 check :: KleisliQueue m a b -> KleisliQueue m a b
-check (KleisliQueue lenf f lenr r) = undefined
+check (KleisliQueue lenh h lent t)
+    | lenh > lent = _
